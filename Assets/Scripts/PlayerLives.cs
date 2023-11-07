48ace765e167bcdfec407f9fe41cf2c95ed3f793
SarojKapali <81839032+saroj-kapali@users.noreply.github.com>
2023-11-06 21:00:47 -0600

    jump-and-life-sound
    jump-and-life-sound-added

--- Assets/Scripts/PlayerLives.cs
+++ Assets/Scripts/PlayerLives.cs
@@ -5,16 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class PlayerLives : MonoBehaviour
 {
-    public List<Image> playerHeads; // Drag the three UI Image components representing the player heads
-    public string gameOverSceneName = "GameOverScene"; // Name of the game over scene
+    public List<Image> playerHeads;
+    public string gameOverSceneName = "GameOverScene";
+    public AudioClip lifeLostSound; // Assign the sound effect to this variable in the Inspector
+    public float pitchDecreaseRate = 0.1f; // Rate at which the pitch decreases
 
     private int lives;
-    private HashSet<GameObject> hitObstacles = new HashSet<GameObject>(); // To keep track of hit obstacles
+    private HashSet<GameObject> hitObstacles = new HashSet<GameObject>();
+    private AudioSource audioSource;
 
     private void Start()
     {
-        lives = playerHeads.Count; // Start with the number of images you added
+        lives = playerHeads.Count;
         UpdateLivesDisplay();
+
+        // Get the AudioSource component attached to the same GameObject
+        audioSource = GetComponent<AudioSource>();
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -29,8 +35,19 @@ public class PlayerLives : MonoBehaviour
     private void ReduceLife()
     {
         lives--;
-
         UpdateLivesDisplay();
+
+        // Play the sound effect with decreasing pitch
+        if (lifeLostSound != null && audioSource != null)
+        {
+            audioSource.clip = lifeLostSound;
+            audioSource.pitch -= pitchDecreaseRate;
+            if (audioSource.pitch < 0f)
+            {
+                audioSource.pitch = 0f;
+            }
+            audioSource.Play();
+        }
 
         if (lives <= 0)
         {
@@ -40,7 +57,6 @@ public class PlayerLives : MonoBehaviour
 
     private void UpdateLivesDisplay()
     {
-        // Hide one player head image for each lost life
         for (int i = 0; i < playerHeads.Count; i++)
         {
             if (i < lives)
@@ -56,13 +72,11 @@ public class PlayerLives : MonoBehaviour
 
     private void TransitionToGameOver()
     {
-        // Stop the timer and record the final time
-        if (Stopwatch.instance != null) // Ensure there's an instance available
+        if (Stopwatch.instance != null)
         {
             Stopwatch.instance.StopTimer();
         }
 
         SceneManager.LoadScene(gameOverSceneName);
     }
-
 }
